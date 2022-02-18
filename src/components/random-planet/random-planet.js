16ac4dbeb9e6106c3c16be03ed3d9f120ebf506d
Abdumalik Kochkorov <theabdyko@gmail.com>
2022-02-19 00:32:40 +0600

    lesson 9

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,31 +1,65 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
+import SwapiService from '../../services/swapi-service';
 import './random-planet.css';
 
 
-export default class RandomPlanet extends Component {
-  render() {
-    return (
-      <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
-             src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
-        <div>
-          <h4>Planet Name</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>123124</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>43</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>100</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
+const Loader = () => <h1>Loading...</h1>
+const Loader = () => <h1>Something went wrong...</h1>
+
+
+const RandomPlanet = () => {
+  const [data, setData] = useState({
+    loading: true,
+    error: false,
+  })
+  const swapi = new SwapiService()
+
+  useEffect(() => {
+    const planetInterval = setInterval(() => {
+      const id = Math.floor(Math.random() * (20 - 1) + 1);
+      swapi.getPlanet(id).then(planet => {
+        setData({...data, ...planet, loading: false, error: false})
+      }).catch(error => {
+        setData({...data, loading: false, error: true})
+      })
+    }, 2500)
+
+    return () => clearInterval(planetInterval)
+  }, [])
+
+  const {id, name, population, rotationPeriod, diameter} = data;
+  const imageUrl = `https://starwars-visualguide.com/assets/img/planets/${id}.jpg`
+
+  if (data.loading) {
+      return <Loader />
   }
+
+  if (data.error) {
+      return <Error />
+  }
+
+  return (
+    <div className="random-planet jumbotron rounded">
+      <img className="planet-image" src={imageUrl} />
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
+
+export default RandomPlanet;
